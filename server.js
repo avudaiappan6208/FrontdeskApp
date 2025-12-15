2bdd18d15847ff046d75183f2a90b57dda3eb0c5
Avudaiappan-FSD <harinzz6803@gmail.com>
2025-12-15 21:48:19 +0530

    database connection is done

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from server!' });
@@ -6,5 +7,10 @@ app.get('/', (req, res) => {
 app.listen(3001, () => {
     console.log('Server is running @ http://localhost:3001');
 });
-
-
+mongoose.connect(`mongodb+srv://vishal:vishalr6803@atlas.ib7eqcx.mongodb.net/FrontDeskApp`)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
