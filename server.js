e6d87b25a8569dda7ede6c868561be1c732b38d9
Avudaiappan-FSD <harinzz6803@gmail.com>
2025-12-15 22:34:56 +0530

    config.js excercise done!!

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { mongodb_url,PORT } = require('./utils/config');
+const { mongodb_url } = require('./utils/config');
 require('dotenv').config();
 const app = express();
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from server!' });
 });
-app.listen(PORT, () => {
+app.listen(3001, () => {
     console.log('Server is running @ http://localhost:3001');
 });
 mongoose.connect(mongodb_url )
--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 const mongodb_url = process.env.Mongodb;
-const PORT = process.env.PORT;
+
 module.exports = {
     mongodb_url,
-    PORT
+    
 };
