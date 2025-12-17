9a5ca2125839450c5109d95fbec97299a143e500
Avudaiappan-FSD <harinzz6803@gmail.com>
2025-12-18 01:34:15 +0530

    register endpoint except bcrypt

--- Controller/Authcontroller.js
+++ Controller/Authcontroller.js
@@ -4,11 +4,18 @@ const authcontroller = {
     register: async(req, res) => {
         try{
             const { username, email, password} = req.body;
+
+            const user = await User.findOne({email});
+            if(user){
+                return res.status(400).json({message: 'Email already in use'});
+            }
+
             const newuser = new User({
                 username,
                 email,
                 password
             });
+
             await newuser.save();
             res.status(201).json({message: 'User registered successfully'});
         }catch(error){
